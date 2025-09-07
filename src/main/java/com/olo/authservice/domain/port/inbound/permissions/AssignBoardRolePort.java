f1109f1ed5222f15524e7869f325753116df505c
Oswaldo Lopez <77045040+RomperS@users.noreply.github.com>
2025-09-07 22:45:10 +0000

    feat(domain): add inbound ports for assigning and revoking boardRole

---
+++ src/main/java/com/olo/authservice/domain/port/inbound/permissions/AssignBoardRolePort.java
@@ -0,0 +1,5 @@
+package com.olo.authservice.domain.port.inbound.permissions;
+
+public interface AssignBoardRolePort {
+    void assignBoardRole(Long userId);
+}
---
+++ src/main/java/com/olo/authservice/domain/port/inbound/permissions/RevokeBoardRolePort.java
@@ -0,0 +1,5 @@
+package com.olo.authservice.domain.port.inbound.permissions;
+
+public interface RevokeBoardRolePort {
+    void revokeBoardRole(Long userId);
+}
