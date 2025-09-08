6a734b007abe5ed55c74a40088c0810111a4670e
Oswaldo Lopez <77045040+RomperS@users.noreply.github.com>
2025-09-08 21:15:00 +0000

    feat(CreateUserCommand): Add email attribute and role and title validation ADMIN:SECRETARY

--- src/main/java/com/olo/authservice/domain/command/CreateUserCommand.java
+++ src/main/java/com/olo/authservice/domain/command/CreateUserCommand.java
@@ -6,18 +6,18 @@ import com.olo.permissions.Role;
 import com.olo.permissions.Title;
 
 public record CreateUserCommand(
+        String email,
         Role role,
         Title title
 ) {
     public static CreateUserCommand of(CreateUserCommand command) {
-        if (command.role.equals(Role.SUPER_ADMIN)) {
+        if (command.role.equals(Role.SUPER_ADMIN) || (command.role.equals(Role.ADMIN) && command.title.equals(Title.SECRETARY))) {
             throw new SuperAdminCreationDeniedException("Creating a user with super-administrator permissions is not allowed.");
         }
         Title finalTitle = (command.title != null) ? command.title : command.role.getDefaultTitle();
         if (!finalTitle.getRole().equals(command.role)) {
             throw new InvalidPermissionValueException("Title and role do not match, invalid values");
         }
-        return new CreateUserCommand(command.role, finalTitle);
+        return new CreateUserCommand(command.email(), command.role, finalTitle);
     }
-
 }
