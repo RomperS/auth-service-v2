1466e1f26321655b6c76d6788e97d737aec5a207
Oswaldo Lopez <77045040+RomperS@users.noreply.github.com>
2025-09-08 21:41:42 +0000

    refactor(User): Add sendMail to CreateUserImpl to send registration completion email

--- src/main/java/com/olo/authservice/application/usecase/users/CreateUserImpl.java
+++ src/main/java/com/olo/authservice/application/usecase/users/CreateUserImpl.java
@@ -1,10 +1,13 @@
 package com.olo.authservice.application.usecase.users;
 
 import com.olo.authservice.anotations.CustomTransactional;
+import com.olo.authservice.application.service.TokenService;
 import com.olo.authservice.domain.command.CreateUserCommand;
+import com.olo.authservice.domain.command.EmailCommand;
 import com.olo.authservice.domain.model.User;
 import com.olo.authservice.domain.port.inbound.users.CreateUserPort;
 import com.olo.authservice.domain.port.outbound.CommonStringPort;
+import com.olo.authservice.domain.port.outbound.EmailServicePort;
 import com.olo.authservice.domain.port.outbound.UserRepositoryPort;
 import lombok.RequiredArgsConstructor;
 
@@ -15,6 +18,8 @@ public class CreateUserImpl implements CreateUserPort {
 
     private final UserRepositoryPort userRepositoryPort;
     private final CommonStringPort commonStringPort;
+    private final EmailServicePort emailServicePort;
+    private final TokenService tokenService;
 
     @Override
     @CustomTransactional
@@ -33,6 +38,29 @@ public class CreateUserImpl implements CreateUserPort {
                 List.of(createUserCommand.title())
         );
 
-        return userRepositoryPort.save(user);
+        User savedUser = userRepositoryPort.save(user);
+
+        String activateToken = tokenService.generateActivationToken(savedUser.id());
+
+        sendMail(activateToken, command.email());
+
+        return savedUser;
+    }
+
+    public void sendMail(String activationToken, String email) {
+        String baseUrl = "http://127.0.0.1:5500/index.html?token=";
+
+        String finalLink = baseUrl + "?token=" + activationToken;
+
+        String subject = "Finalizar registro";
+        String body = "Para terminar su registro, haga clic en el siguiente enlace: <a href='" + finalLink + "'>TERMINAR REGISTRO</a>";
+
+        EmailCommand emailCommand = new EmailCommand(
+                email,
+                subject,
+                body
+        );
+
+        emailServicePort.sendEmail(emailCommand);
     }
 }
