78ab29c31d9162a7efa30ae3ef37477d12f3dfe1
Oswaldo Lopez <77045040+RomperS@users.noreply.github.com>
2025-09-08 04:28:00 +0000

    feat(auth): emit event during SignupImpl execution

--- src/main/java/com/olo/authservice/application/usecase/validation/SignupImpl.java
+++ src/main/java/com/olo/authservice/application/usecase/validation/SignupImpl.java
@@ -2,18 +2,23 @@ package com.olo.authservice.application.usecase.validation;
 
 import com.olo.authservice.application.service.TokenService;
 import com.olo.authservice.application.service.UserService;
+import com.olo.authservice.domain.command.PublishCredentialsCommand;
 import com.olo.authservice.domain.command.SignupCommand;
 import com.olo.authservice.domain.command.UpdateUserCommand;
 import com.olo.authservice.domain.exception.token.InvalidTokenException;
+import com.olo.authservice.domain.exception.user.MalformedRecordException;
 import com.olo.authservice.domain.model.Token;
 import com.olo.authservice.domain.model.User;
 import com.olo.authservice.domain.port.inbound.validation.SignupPort;
 import com.olo.authservice.domain.port.outbound.JwtServicePort;
+import com.olo.authservice.domain.port.outbound.KafkaServicePort;
 import com.olo.authservice.domain.result.AccessTokenResult;
 import com.olo.authservice.domain.result.AuthResult;
+import com.olo.permissions.Role;
 import lombok.RequiredArgsConstructor;
 
 import java.time.Instant;
+import java.util.List;
 
 @RequiredArgsConstructor
 public class SignupImpl implements SignupPort {
@@ -21,6 +26,7 @@ public class SignupImpl implements SignupPort {
     private final UserService userService;
     private final TokenService tokenService;
     private final JwtServicePort  jwtServicePort;
+    private final KafkaServicePort kafkaServicePort;
 
     @Override
     public AuthResult signup(SignupCommand command, String token) {
@@ -45,6 +51,18 @@ public class SignupImpl implements SignupPort {
         String accessToken = tokenService.generateAccessToken(refreshToken.refreshToken());
         Instant expireAt = Instant.now().plusMillis(jwtServicePort.getAccessTokenExpiration());
 
+        List<Role> roles = updatedUser.roles().stream().filter(c -> c != com.olo.permissions.Role.AUXILIARY_ADMIN).toList();
+        if (roles.size() != 1){
+            throw new MalformedRecordException("Invalid roles");
+        }
+
+        PublishCredentialsCommand publishCredentialsCommand = new PublishCredentialsCommand(
+                userId.toString(),
+                roles.get(0).toString()
+        );
+
+        registerCredentials(publishCredentialsCommand);
+
         return new AuthResult(
                 refreshToken.refreshToken(),
                 refreshToken.expiredAt(),
@@ -53,5 +71,9 @@ public class SignupImpl implements SignupPort {
                         expireAt
                 )
         );
+    }
+
+    private void registerCredentials(PublishCredentialsCommand command) {
+        kafkaServicePort.publishCredentialsCreated(command);
     }
 }
