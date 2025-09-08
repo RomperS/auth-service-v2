62c07e906dbf0801ecb6ba59b74a29106c60a23b
Oswaldo Lopez <77045040+RomperS@users.noreply.github.com>
2025-09-08 22:05:17 +0000

    refactor(SecurityConfig): Modification to public endpoints and validation of other endpoints

--- src/main/java/com/olo/authservice/infrastructure/config/SecurityConfig.java
+++ src/main/java/com/olo/authservice/infrastructure/config/SecurityConfig.java
@@ -4,9 +4,7 @@ import com.olo.authservice.infrastructure.security.CustomAuthenticationEntryPoin
 import com.olo.authservice.infrastructure.security.CustomUserDetailsService;
 import com.olo.authservice.infrastructure.security.JwtAuthFilter;
 import com.olo.authservice.infrastructure.security.JwtServiceImpl;
-import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -14,6 +12,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @RequiredArgsConstructor
@@ -36,10 +37,11 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http, JwtAuthFilter jwtAuthFilter) throws Exception {
         http
                 .userDetailsService(userDetailsService)
+                .cors(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/validation/login", "/validation/logout", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
-                        .requestMatchers("/validation/signup").hasAnyAuthority("ROLE_SUPER_ADMIN", "TITLE_SECRETARY")
+                        .requestMatchers("/users/**","/validation/login", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                        .requestMatchers("/validation/signup", "validation/token", "validation/logout").authenticated()
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
