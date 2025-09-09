f153ab7c5fa1eee3cae7bc6f5d17eaa1703d25a3
Oswaldo Lopez <77045040+RomperS@users.noreply.github.com>
2025-09-09 17:30:16 +0000

    feat(Kafka): Add consumer service with topic createdUser

---
+++ src/main/java/com/olo/authservice/infrastructure/adapters/kafka/KafkaConsumerService.java
@@ -0,0 +1,43 @@
+package com.olo.authservice.infrastructure.adapters.kafka;
+
+import com.olo.authservice.application.service.UserService;
+import com.olo.authservice.domain.command.CreateUserCommand;
+import com.olo.authservice.domain.model.User;
+import com.olo.permissions.Role;
+import com.olo.permissions.Title;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class KafkaConsumerService {
+
+    private final UserService userService;
+
+    @KafkaListener(topics = "createdUser", groupId = "auth-service-group")
+    public void createCredentials(String msg){
+        String regex = "User with email '(.+?)', role '(.+?)' and title '(.+?)' created\\.";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(msg);
+
+        if (matcher.find()) {
+            String email = matcher.group(1);
+            String role = matcher.group(2);
+            String title = matcher.group(3);
+            CreateUserCommand command = new CreateUserCommand(
+                    email,
+                    Role.fromString(role),
+                    Title.fromString(title)
+            );
+            User createdUser = userService.createUser(command);
+
+            log.info("Create credentials: {}", createdUser);
+        }
+    }
+}
