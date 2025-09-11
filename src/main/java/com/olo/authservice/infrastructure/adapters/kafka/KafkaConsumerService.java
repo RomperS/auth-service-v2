0b98a0ef57ca502c1e8f137dd7370847aab7fb5d
Oswaldo Lopez <77045040+RomperS@users.noreply.github.com>
2025-09-11 23:17:59 +0000

    feat: enhance KafkaConsumerService with dni parsing, validation, and compensation event publishing on errors

--- src/main/java/com/olo/authservice/infrastructure/adapters/kafka/KafkaConsumerService.java
+++ src/main/java/com/olo/authservice/infrastructure/adapters/kafka/KafkaConsumerService.java
@@ -2,7 +2,9 @@ package com.olo.authservice.infrastructure.adapters.kafka;
 
 import com.olo.authservice.application.service.UserService;
 import com.olo.authservice.domain.command.CreateUserCommand;
-import com.olo.authservice.domain.model.User;
+import com.olo.authservice.domain.exception.kafka.InvalidMessageFormatException;
+import com.olo.authservice.domain.port.outbound.KafkaProducerPort;
+import com.olo.exceptions.DomainException;
 import com.olo.permissions.Role;
 import com.olo.permissions.Title;
 import lombok.RequiredArgsConstructor;
@@ -19,25 +21,45 @@ import java.util.regex.Pattern;
 public class KafkaConsumerService {
 
     private final UserService userService;
+    private final KafkaProducerPort kafkaProducerPort;
 
     @KafkaListener(topics = "created-user", groupId = "auth-service-group")
     public void createCredentials(String msg){
-        String regex = "User with email '(.+?)', role '(.+?)' and title '(.+?)' created\\.";
-        Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(msg);
+        String dni = "";
+        try{
+            String regex = "User with email '(.+?)', role '(.+?)', title '(.+?)' and dni '(.+?)' created";
+            Pattern pattern = Pattern.compile(regex);
+            Matcher matcher = pattern.matcher(msg);
+            if (matcher.find()) {
+                String email = matcher.group(1);
+                String role = matcher.group(2);
+                String title = matcher.group(3);
+                dni = matcher.group(4);
+                Long longDni = Long.parseLong(dni);
 
-        if (matcher.find()) {
-            String email = matcher.group(1);
-            String role = matcher.group(2);
-            String title = matcher.group(3);
-            CreateUserCommand command = new CreateUserCommand(
-                    email,
-                    Role.fromString(role),
-                    Title.fromString(title)
-            );
-            User createdUser = userService.createUser(command);
-
-            log.info("Create credentials: {}", createdUser);
+                CreateUserCommand command = new CreateUserCommand(
+                        email,
+                        longDni,
+                        Role.fromString(role),
+                        Title.fromString(title)
+                );
+                userService.createUser(command);
+            }else {
+                throw new InvalidMessageFormatException("The message does not match the expected format.");
+            }
+        } catch (DomainException e) {
+            log.error("An expected domain exception occurred: {}", e.getMessage(), e);
+            createCredentialsCompensationEvent(e.getMessage(), dni);
+            throw e;
+        } catch (Exception e) {
+            log.error("An unexpected error occurred during message processing.", e);
+            createCredentialsCompensationEvent(e.getMessage(), dni);
+            throw new RuntimeException("Unexpected error during user creation.", e);
         }
+    }
+
+    private void createCredentialsCompensationEvent(String exceptionMsg, String dni) {
+        Long longDni = Long.parseLong(dni);
+        kafkaProducerPort.createCompensationEvent(exceptionMsg, longDni);
     }
 }
