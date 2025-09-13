c0cc33c4a8b10cc30c1feb12765cfd7a4bc7661a
Oswaldo Lopez <77045040+RomperS@users.noreply.github.com>
2025-09-13 21:48:15 +0000

    fix(jwt): decode Base64 secret key before creating HMAC key

--- src/main/java/com/olo/authservice/infrastructure/security/JwtServiceImpl.java
+++ src/main/java/com/olo/authservice/infrastructure/security/JwtServiceImpl.java
@@ -8,6 +8,7 @@ import com.olo.authservice.infrastructure.repositories.JpaTokenRepository;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
@@ -17,7 +18,6 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
-import java.nio.charset.StandardCharsets;
 import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
@@ -42,7 +42,7 @@ public class JwtServiceImpl implements JwtServicePort {
 
     @PostConstruct
     public void init() {
-        key = Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
+        key = Keys.hmacShaKeyFor(Decoders.BASE64.decode(secretKey));
     }
 
     @Override
